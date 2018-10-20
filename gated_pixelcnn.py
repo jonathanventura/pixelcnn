488e0aa176998e7f32769f104c011966a2016160
Jonathan Ventura <jventu09@calpoly.edu>
2018-10-20 05:05:03 +0000

    fixing up gated pixelcnn

--- gated_pixelcnn.py
+++ gated_pixelcnn.py
@@ -3,6 +3,16 @@ import tensorflow as tf
 import numpy as np
 from math import floor, ceil
 
+def _conv(inputs, num_outputs, filter_size, name):
+    # relu activation
+    x = tf.nn.relu(inputs)
+
+    # convolution
+    x = tf.layers.conv2d(x, num_outputs, filter_size,
+            padding='valid', activation=None, name=name)
+
+    return x
+
 def _get_bias(h,num_filters,name):
     bias = tf.layers.dense(h, 2*num_filters, name=name,
         activation=None, use_bias=False)
@@ -18,7 +28,7 @@ def _apply_activation(x,num_filters):
     # combine activations
     return x_tanh * x_sigmoid
 
-def _pixel_cnn_layer(vinput,hinput,filter_size,num_filters,layer_index,h=None):
+def _gated_pixel_cnn_layer(vinput,hinput,filter_size,num_filters,layer_index,h=None):
     """Gated activation PixelCNN layer
        Paper reference: https://arxiv.org/pdf/1606.05328.pdf
        Code reference: https://github.com/dritchie/pixelCNN/blob/master/pixelCNN.lua
@@ -29,8 +39,7 @@ def _pixel_cnn_layer(vinput,hinput,filter_size,num_filters,layer_index,h=None):
     
     # kxk convolution for vertical stack
     vinput_padded = tf.pad(vinput, [[0,0],[ceilk,0],[floork,floork],[0,0]])
-    vconv = tf.layers.conv2d(vinput_padded, 2*num_filters, [ceilk,k], name='vconv_%d'%layer_index,
-        padding='valid', activation=None)
+    vconv = _conv(vinput_padded, 2*num_filters, [ceilk,k], 'vconv_%d'%layer_index)
     vconv = vconv[:,:-1,:,:]
     
     # bias for vertical stack
@@ -39,8 +48,7 @@ def _pixel_cnn_layer(vinput,hinput,filter_size,num_filters,layer_index,h=None):
 
     # kx1 convolution for horizontal stack
     hinput_padded = tf.pad(hinput, [[0,0],[0,0],[ceilk,0],[0,0]])
-    hconv = tf.layers.conv2d(hinput_padded, 2*num_filters, [1,ceilk], name='hconv_%d'%layer_index,
-        padding='valid', activation=None)
+    hconv = _conv(hinput_padded, 2*num_filters, [1,ceilk], 'hconv_%d'%layer_index)
     if layer_index==0:
         hconv = hconv[:,:,:-1,:]
     else:
@@ -51,8 +59,7 @@ def _pixel_cnn_layer(vinput,hinput,filter_size,num_filters,layer_index,h=None):
         hconv += _get_bias(h,num_filters,'hbias_%d'%layer_index)
     
     # 1x1 transitional convolution for vstack
-    vconv1 = tf.layers.conv2d(vconv, 2*num_filters, 1, name='vconv1_%d'%layer_index,
-        padding='valid', activation=None)
+    vconv1 = _conv(vconv, 2*num_filters, 1, 'vconv1_%d'%layer_index)
     
     # add vstack to hstack
     hconv = hconv + vconv1
@@ -63,14 +70,13 @@ def _pixel_cnn_layer(vinput,hinput,filter_size,num_filters,layer_index,h=None):
  
     # residual connection in hstack
     if layer_index > 0:
-        hconv1 = tf.layers.conv2d(hconv, num_filters, 1, name='hconv1_%d'%layer_index,
-            padding='valid', activation=None)
+        hconv1 = _conv(hconv, num_filters, 1, 'hconv1_%d'%layer_index)
         hconv = hinput + hconv1
     
     return vconv, hconv
 
-def pixel_cnn(inputs,num_filters,num_layers,output_dim,h=None):
-    """Builds PixelCNN graph.
+def gated_pixelcnn(inputs,num_filters,num_layers,output_dim,h=None):
+    """Builds Gated PixelCNN graph.
     Args:
         inputs: input tensor (B,H,W,C)
         h: optional (B,K) tensor to condition the model on
@@ -82,18 +88,15 @@ def pixel_cnn(inputs,num_filters,num_layers,output_dim,h=None):
         hstack = inputs
         
         # first layer: masked 7x7
-        vstack, hstack = _pixel_cnn_layer(vstack,hstack,7,num_filters,0,h=h)
+        vstack, hstack = _gated_pixel_cnn_layer(vstack,hstack,7,num_filters,0,h=h)
 
         # next layers: masked 3x3
         for i in range(num_layers):
-            vstack, hstack = _pixel_cnn_layer(vstack,hstack,3,num_filters,i+1,h=h)
+            vstack, hstack = _gated_pixel_cnn_layer(vstack,hstack,3,num_filters,i+1,h=h)
         
         # final layers
-        x = tf.nn.relu(hstack)
-        x = tf.layers.conv2d(x, num_filters, 1, name='conv',
-            padding='valid', activation=tf.nn.relu)
-        logits = tf.layers.conv2d(x, output_dim, 1, name='pred',
-            padding='valid', activation=None)
+        x = _conv(hstack, num_filters, 1, 'conv')
+        logits = _conv(x, output_dim, 1, 'pred')
         
     return logits
 
