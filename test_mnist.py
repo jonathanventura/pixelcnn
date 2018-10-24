bb07c8bc6c77790bfb49135a8bfcccd3d710b66d
Jonathan Ventura <jventu09@calpoly.edu>
2018-10-24 04:10:43 +0000

    Sprucing up mnist test script

--- test_mnist.py
+++ test_mnist.py
@@ -1,3 +1,4 @@
+import argparse
 import tensorflow as tf
 import numpy as np
 from model import BinaryModel
@@ -5,23 +6,47 @@ from tensorpack import *
 from tensorpack.utils.viz import stack_patches
 import sys
 import cv2
+from train_mnist import binarize, get_mnist
 
-def binarize(x):
-    return x > np.random.sample(x.shape)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate MNIST images')
+    parser.add_argument('checkpoint', help='path to checkpoint')
+    parser.add_argument('--complete', action='store_true', help='complete test images')
+    args = parser.parse_args()
 
-pred_config = PredictConfig(
-    model=BinaryModel(),
-    input_names=['image'],
-    output_names=['probs'],
-    session_init=SaverRestore(sys.argv[1])
-)
-predictor = OfflinePredictor(pred_config)
+    # set up model configuration
+    pred_config = PredictConfig(
+        model=BinaryModel(),
+        input_names=['image'],
+        output_names=['probs'],
+        session_init=SaverRestore(sys.argv[1])
+    )
+    predictor = OfflinePredictor(pred_config)
 
-images = np.zeros((10,28,28),dtype='float32')
-for y in range(28):
-    for x in range(28):
-        probs, = predictor(images)
-        sample = probs > np.random.sample(probs.shape)
-        images[:,y,x] = sample[:,y,x]
-imout = stack_patches(images,2,5)
-cv2.imwrite('stack.png',imout)
+    # initialize images to zeros
+    images = np.zeros((25,28,28),dtype='float32')
+
+    # grab top half of test images if necessary
+    if args.complete:
+        print('getting test images')
+        ds_test = get_mnist('test',1,False,True)
+        i = 0
+        for dp in ds_test:
+            images[i] = dp[0]
+            i = i + 1
+            if i == len(images): break
+        # zero out bottom half
+        images[:,14:] = 0
+
+    # run sequential prediction
+    ystart = 14 if args.complete else 0
+    for y in range(ystart,28):
+        for x in range(28):
+            probs, = predictor(images)
+            sample = probs > np.random.sample(probs.shape)
+            images[:,y,x] = sample[:,y,x]
+
+    # generate output stack
+    imout = stack_patches(images*255,5,5)
+    cv2.imwrite('stack.png',imout)
+
--- train_mnist.py
+++ train_mnist.py
@@ -18,21 +18,22 @@ def get_data():
     ds_test = get_mnist('test',16,False,True)
     return ds_train, ds_test
 
-ds_train, ds_test = get_data()
+if __name__ == '__main__':
+    ds_train, ds_test = get_data()
 
-logger.auto_set_dir()
+    logger.auto_set_dir()
 
-steps_per_epoch = len(ds_train)
-train_config = AutoResumeTrainConfig(
-    model=BinaryModel(),
-    data=FeedInput(ds_train),
-    callbacks=[
-        ModelSaver(),
-        MinSaver('validation_loss'),
-        InferenceRunner(ds_test,[ScalarStats(['loss'])])
-    ],
-    steps_per_epoch=steps_per_epoch,
-    max_epoch=200,
-)
-launch_train_with_config(train_config, SimpleTrainer())
+    steps_per_epoch = len(ds_train)
+    train_config = AutoResumeTrainConfig(
+        model=BinaryModel(),
+        data=FeedInput(ds_train),
+        callbacks=[
+            ModelSaver(),
+            MinSaver('validation_loss'),
+            InferenceRunner(ds_test,[ScalarStats(['loss'])])
+        ],
+        steps_per_epoch=steps_per_epoch,
+        max_epoch=200,
+    )
+    launch_train_with_config(train_config, SimpleTrainer())
 
